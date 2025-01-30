7b3a2b7112ac69df5f0d0beac64880252b2e55f1
Zenk41 <ardhidhani@gmail.com>
2025-01-30 22:08:28 +0700

    feat: Add WebRTC signaling events
    - Introduced new event constants:
      - `EventJoinRoom`
      - `EventRoomInfo`
      - `EventNewPeer`
      - `EventOffer`
      - `EventAnswer`
      - `EventIceCandidate`
    - Added new event structures:
      - `JoinRoomEvent`: Represents a user joining a room.
      - `RoomInfoEvent`: Contains room details and users list.
      - `NewPeerEvent`: Signals a new peer joining a room.
      - `OfferEvent`: Handles WebRTC offer exchange.
      - `AnswerEvent`: Handles WebRTC answer exchange.
      - `Candidate`: Defines ICE candidate attributes.
      - `IceCandidateEvent`: Handles ICE candidate exchange.
    
    These additions enhance the signaling mechanism for WebRTC peer connections.

--- chat/event.go
+++ chat/event.go
@@ -13,9 +13,15 @@ type Event struct {
 type EventHandler func(event Event, c *Client) error
 
 const (
-	EventSendMessage = "send_message"
-	EventNewMessage  = "new_message"
-	EventChangeRoom  = "change_room"
+	EventSendMessage  = "send_message"
+	EventNewMessage   = "new_message"
+	EventChangeRoom   = "change_room"
+	EventJoinRoom     = "join_room"
+	EventRoomInfo     = "room_info"
+	EventNewPeer      = "new_peer"
+	EventOffer        = "offer"
+	EventAnswer       = "answer"
+	EventIceCandidate = "ice_candidate"
 )
 
 type SendMessageEvent struct {
@@ -30,4 +36,49 @@ type NewMessageEvent struct {
 
 type ChangeRoomEvent struct {
 	Name string `json:"name"`
+}
+
+type JoinRoomEvent struct {
+	Type   string `json:"type"`
+	Room   string `json:"room"`
+	UserId string `json:"user_id"`
+}
+
+type RoomInfoEvent struct {
+	Type  string   `json:"type"`
+	Room  string   `json:"room"`
+	Users []string `json:"users"`
+}
+
+type NewPeerEvent struct {
+	Type   string `json:"type"`
+	Room   string `json:"room"`
+	UserId string `json:"user_id"`
+}
+
+type OfferEvent struct {
+	Type string `json:"type"`
+	From string `json:"from"`
+	To   string `json:"to"`
+	Sdp  string `json:"sdp"`
+}
+
+type AnswerEvent struct {
+	Type string `json:"type"`
+	From string `json:"from"`
+	To   string `json:"to"`
+	Sdp  string `json:"sdp"`
+}
+
+type Candidate struct {
+	Candidate     string `json:"candidate"`
+	SdpMid        string `json:"sdp_mid"`
+	SdpMLineIndex int    `json:"sdp_m_line_index"`
+}
+
+type IceCandidateEvent struct {
+	Type      string    `json:"type"`
+	From      string    `json:"from"`
+	To        string    `json:"to"`
+	Candidate Candidate `json:"candidate"`
 }
